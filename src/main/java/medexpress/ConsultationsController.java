09ba9eb9c4c23d19fb9a075a0f55dfb1c362424e
mendezdemian <mendezdemian@gmail.com>
2026-02-16 22:07:46 +0000

    initial working controller

---
+++ src/main/java/medexpress/ConsultationsController.java
@@ -0,0 +1,20 @@
+package medexpress;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/consultation")
+public class ConsultationsController {
+    @GetMapping(
+            path = "/questionnaire/{code}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> retrieveQuestionnaire(@PathVariable("code") String code) {
+        return ResponseEntity.ok().body(code);
+    }
+}
---
+++ src/main/java/medexpress/MedExpressApp.java
@@ -0,0 +1,11 @@
+package medexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedExpressApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MedExpressApp.class, args);
+    }
+}
